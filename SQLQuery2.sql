14f43d5c959c4c027332494c64c0c04487682383
ValterSilvaJr <valtersilvajr@live.com>
2020-06-01 06:36:24 -0300

    normalização dos relacionamentos entre as tabelas USUARIO, ENDERECO, CLIENTE e PESSOA_FISICA adicionando a função ON DELETE CASCADE

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,23 +1,45 @@
 ﻿
 select * from USUARIO
 select * from ENDERECO
-insert into USUARIO(nome, email, senha) values('Valter Silva','valter@mail.com','12345678');
-insert into ENDERECO(logradouro, numero, cep, bairro, cidade, uf, pais, id_usuario) values('Rua dos Bobos','0','00000000','Praça','São Paulo','SP','Brasil', @@IDENTITY);
-/*insert into ENDERECO(logradouro, numero, cep, bairro, cidade, uf, pais, id_endereco) 
-values('TEST','255','0444','ASSA','SASASASAS','SS','BR', @id_usuario)*/
+select * from CLIENTE
+select * from PESSOA_FISICA
 
+/*Insere dados nas tabelas relacionadas*/
+insert into USUARIO(nome, email, senha) values('Valter','valter@mail.com','12345678');
+insert into ENDERECO(logradouro, numero, cep, bairro, cidade, uf, pais, id_usuario) values('Rua Dois','85B','50882-059','Townsville','Luganenhum','MG','Multiverso', @@IDENTITY);
+insert into CLIENTE(id_usuario, tipo) values(@@IDENTITY, 'PF')
+insert into PESSOA_FISICA(id_cliente,cpf, dataNasc) values( @@IDENTITY,'32145678800','2010-05-25')
+
+/*TESTES*/
 select u.nome, e.logradouro, e.numero from USUARIO u
-inner join ENDERECO e
-on u.id_endereco != ''
+inner join ENDERECO e on e.id_endereco = u.id_usuario
 
 select u.id_usuario, u.nome, e.logradouro, e.numero, e.id_endereco from USUARIO u
-inner join ENDERECO e
-on u.id_usuario = e.id_usuario
+inner join ENDERECO e on u.id_usuario = e.id_usuario
 order by id_usuario
 
 select u.id_usuario, u.nome, e.logradouro, e.numero from ENDERECO e
-inner join USUARIO u
-on e.id_usuario = u.id_usuario
+inner join USUARIO u on e.id_usuario = u.id_usuario
 
-dbcc checkident (endereco, reseed, 0) /*Comando para resetar a tabela*/
-select IDENT_CURRENT('USUARIO') /*Comando para exibir o id atual da tabela*/
+select * from USUARIO where email like 'v%'
+
+/*Faz uma busca nas tabelas relacionadas*/
+select u.id_usuario 'ID', c.id_cliente 'ID CLIENTE', e.id_endereco 'ID END', u.nome 'NOME COMPLETO', pf.cpf 'CPF', pf.dataNasc 'IDADE', e.logradouro 'LOGRADOURO', e.numero 'N', e.bairro 'BAIRRO', e.cidade 'CIDADE', e.uf 'ESTADO', e.pais 'PAÍS' from CLIENTE c
+inner join USUARIO u on u.id_usuario = c.id_usuario
+inner join PESSOA_FISICA pf on pf.id_cliente = c.id_cliente
+inner join ENDERECO e on e.id_usuario = u.id_usuario
+
+/*Comando para resetar a tabela*/
+dbcc checkident (usuario, reseed, 0) 
+dbcc checkident (endereco, reseed, 0)
+dbcc checkident (cliente, reseed, 0)
+
+/*Comando para exibir o id atual da tabela*/
+select IDENT_CURRENT('USUARIO') 
+
+/*MODIFICAÇÕES NAS TABELAS*/
+alter table pessoa_fisica add id_cliente int constraint FK_CLIENTE_PF foreign key(id_cliente) references cliente(id_cliente) on delete cascade;
+alter table CLIENTE add id_usuario int CONSTRAINT FK_ID_USUARIO FOREIGN KEY (id_usuario) REFERENCES USUARIO (id_usuario) on delete cascade;
+alter table endereco add id_usuario int CONSTRAINT FK_ID_USUARIO_ENDERECO FOREIGN KEY (id_usuario) REFERENCES USUARIO (id_usuario) on delete cascade;
+
+delete from USUARIO

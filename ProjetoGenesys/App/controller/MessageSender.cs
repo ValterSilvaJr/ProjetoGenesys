d8ef4581ee9da4b061580304aff36cf8920696b0
ValterSilvaJr <valtersilvajr@live.com>
2020-06-09 23:44:56 -0300

    refatoração do botão de cadastrar dados, tornando possível a especialização do mesmo, ajustes no parâmetro de ocultar os painéis especializados, agora o método recebe 2 parâmetros (nome do painel, tipo de usuario).

---
+++ ProjetoGenesys/App/controller/MessageSender.cs
@@ -0,0 +1,42 @@
+﻿namespace ProjetoGenesys.App.controller
+{
+    public class MessageSender
+    {
+        public class Negative
+        {
+            public static string getMessage(string message)
+            {
+                switch (message)
+                {
+                    case "erroCadastrarDados":
+                        message = "Erro ao cadastrar os Dados";
+                        break;
+                }
+
+                return message;
+            }
+        }
+        public class Positive
+        {
+            public static string getMessage(string message)
+            {
+                switch (message)
+                {
+                    case "sucessoCadastrarDados":
+                        message = "Cadastro realizado com sucesso!";
+                        break;
+                }
+
+                return message;
+            }
+        }
+        public class Caution
+        {
+
+        }
+        public class Danger
+        {
+
+        }
+    }
+}
--- ProjetoGenesys/App/view/FormCadastroUsuario.cs
+++ ProjetoGenesys/App/view/FormCadastroUsuario.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Drawing.Text;
 using System.Runtime.Remoting.Messaging;
 using System.Windows.Forms;
+using ProjetoGenesys.App.controller;
 using ProjetoGenesys.App.model;
 
 namespace ProjetoGenesys.App.view
@@ -12,6 +14,7 @@ namespace ProjetoGenesys.App.view
         PojoPessoaJuridica pojoPj;
         PojoFuncionario pojoFunc;
         UsuarioDao usuarioDao;
+        string inserirUsuario;
         int response;
 
         public FormCadastroUsuario(string tipoUsuario)
@@ -20,36 +23,35 @@ namespace ProjetoGenesys.App.view
             CustomizarAparencia();
 
             // O formulário recebe um parâmetro com o tipo de usuário que será inserido no sistema e exibe o painél especializado
+
             switch (tipoUsuario)
             {
                 case "PF":
-                    ExibirPainelEspecializacao(pnlPF);
+                    ExibirPainelEspecializacao(pnlPF, "PF");
                     break;
                 case "PJ":
-                    ExibirPainelEspecializacao(pnlPJ);
+                    ExibirPainelEspecializacao(pnlPJ, "PJ");
                     break;
                 case "FCN":
-                    ExibirPainelEspecializacao(pnlCargo);
+                    ExibirPainelEspecializacao(pnlCargo, "FCN");
                     break;
             }
-            
         }
         #region Lógica de ocultar Painéis
 
         private void CustomizarAparencia()
         {
-            /*  Inicialmente todos os painéis são setados para ter a visibilidade
-             *  false através deste método que é carregado junto com o form atual
-             */
+            //Inicialmente todos os painéis são setados para ter a visibilidade false através deste método que é carregado junto com o form atual
+            
             pnlPF.Visible = false;
             pnlPJ.Visible = false;
             pnlCargo.Visible = false;
         }
         private void EsconderPainelEspecializacao()
         {
-            
+
             //É verificado se os painés estão visíveis e caso seja true, os mesmos são setados para false
-             
+
             if (pnlPF.Visible)
             {
                 pnlPF.Visible = false;
@@ -63,13 +65,15 @@ namespace ProjetoGenesys.App.view
                 pnlPJ.Visible = false;
             }
         }
-        private void ExibirPainelEspecializacao(Panel especializacaoPanel)
+        private void ExibirPainelEspecializacao(Panel especializacaoPanel, string tipo)
         {
-            //É passado como parâmetro o painel que ficará visível através deste método, deixando todos os demais invisíveis.
+            //É passado como parâmetro o painel que ficará visível e o tipo de usuário através deste método, deixando todos os demais painéis invisíveis.
+
             if (especializacaoPanel.Visible == false)
             {
                 EsconderPainelEspecializacao();
                 especializacaoPanel.Visible = true;
+                inserirUsuario = tipo;
             }
             else
             {
@@ -81,9 +85,60 @@ namespace ProjetoGenesys.App.view
         {
             Close();
         }
+             
         private void btnInserir_Click(object sender, EventArgs e)
         {
-            
+
+            switch (inserirUsuario)
+            {
+                case "PF":
+                    usuarioDao = new UsuarioDao();
+                    #region PojoUsuario
+                    pojoUsuario = new PojoUsuario();
+                    pojoUsuario.setNome(txtNome.Text);
+                    pojoUsuario.setEmail(txtEmail.Text);
+                    pojoUsuario.setSenha(mskSenha.Text);
+                    pojoUsuario.setLogradouro(txtLogradouro.Text);
+                    pojoUsuario.setNumero(txtNumero.Text);
+                    pojoUsuario.setCep(mskCep.Text);
+                    pojoUsuario.setBairro(txtBairro.Text);
+                    pojoUsuario.setCidade(txtCidade.Text);
+                    pojoUsuario.setUf(txtUf.Text);
+                    pojoUsuario.setPais(txtPais.Text);
+                    #endregion
+
+                    #region PojoPessoaFisica
+                    pojoPf = new PojoPessoaFisica();
+                    pojoPf.setCpf(mskCpf.Text);
+                    pojoPf.setDataNasc(mskDataNasc.Text);
+                    pojoPf.setTipo(inserirUsuario);
+                    #endregion
+
+                    response = usuarioDao.CadastrarUsuario(pojoUsuario, pojoPf);
+
+                    if (response == 1)
+                    {
+                        MessageBox.Show(MessageSender.Positive.getMessage("sucessoCadastrarDados"), "Projeto Genesys", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show(MessageSender.Negative.getMessage("erroCadastrarDados"), "Projeto Genesys", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                    break;
+
+                case "PJ":
+                    MessageBox.Show("PJ", "GENESYS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "FCN":
+                    MessageBox.Show("FCN", "GENESYS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                default:
+                    
+                    break;
+            }
+ 
+            /*
             usuarioDao = new UsuarioDao();
 
             #region PojoUsuario
@@ -124,7 +179,9 @@ namespace ProjetoGenesys.App.view
             pojoFunc.setSetor(txtSetor.Text);
             pojoFunc.setTurno(txtTurno.Text);
             #endregion
-            */
+
+            
+            
             response = usuarioDao.CadastrarUsuario(pojoUsuario, pojoPf);
 
             if(response == 1)
@@ -135,6 +192,7 @@ namespace ProjetoGenesys.App.view
             {
                 MessageBox.Show("Erro ao cadastrar dados", "Projeto Genesys", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+            */
         }
     }
 }

27a76c78f87e0c1ece0f53fc3aee15fe68538577
ValterSilvaJr <valtersilvajr@live.com>
2020-06-17 04:36:02 -0300

    adequação do nome do formulário para ser coerente com o que ele faz

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -6,6 +6,9 @@ select * from PESSOA_FISICA
 select * from PESSOA_JURIDICA
 select * from FUNCIONARIO
 
+
+
+
 /*Insere dados nas tabelas relacionadas*/
 DECLARE @TranName varchar(20);
 SELECT @TranName = 'Inserir Dados PF'
@@ -21,6 +24,9 @@ BEGIN TRANSACTION @TranName
 	insert into PESSOA_FISICA(cpf,id_cliente, dataNasc) values('13177678800', @@IDENTITY, '2010-05-25')
 	
 COMMIT TRANSACTION @TranName
+
+
+
 
 /*TESTES*/
 select u.nome, e.logradouro, e.numero from USUARIO u
@@ -63,20 +69,28 @@ UPDATE FUNCIONARIO
 	SET cargo='C# Developer' FROM FUNCIONARIO f
 	inner join USUARIO u ON u.id_usuario = f.id_usuario WHERE u.id_usuario=2;
 
+
+
+
 /*Faz uma busca nas tabelas relacionadas*/
 select u.id_usuario 'ID', c.id_cliente 'ID CLIENTE', e.id_endereco 'ID END', u.nome 'NOME COMPLETO', pf.cpf 'CPF', pf.dataNasc 'IDADE', e.logradouro 'LOGRADOURO', e.numero 'N', e.bairro 'BAIRRO', e.cidade 'CIDADE', e.uf 'ESTADO', e.pais 'PAÍS' from USUARIO u
 inner join ENDERECO e on e.id_usuario = u.id_usuario
 inner join CLIENTE c on c.id_usuario = u.id_usuario
 inner join PESSOA_FISICA pf on pf.id_cliente = c.id_cliente
 
+
 select u.id_usuario 'ID', c.id_cliente 'ID CLIENTE', e.id_endereco 'ID END', u.nome 'NOME COMPLETO', pj.razao_social 'RAZÃO SOCIAL', pj.nome_fantasia 'NOME FANTASIA', pj.cnpj 'CNPJ', pj.inscricao_estadual 'I.E.', e.logradouro 'LOGRADOURO', e.numero 'N', e.bairro 'BAIRRO', e.cidade 'CIDADE', e.uf 'ESTADO', e.pais 'PAÍS' from USUARIO u
 inner join ENDERECO e on e.id_usuario = u.id_usuario
 inner join CLIENTE c on c.id_usuario = u.id_usuario
 inner join PESSOA_JURIDICA pj on pj.id_cliente = c.id_cliente
 
+
 select u.id_usuario, u.nome, u.email, e.logradouro, e.numero, f.id_funcionario, f.cargo, f.setor, f.turno from USUARIO u
 inner join ENDERECO e ON e.id_usuario = u.id_usuario
 inner join FUNCIONARIO f ON f.id_usuario = u.id_usuario
+
+
+
 
 /*Filtra os usuários da tabela pelo ID*/
 select u.id_usuario 'ID', c.id_cliente 'ID CLIENTE', e.id_endereco 'ID END', u.nome 'NOME COMPLETO', pf.cpf 'CPF', pf.dataNasc 'IDADE', e.logradouro 'LOGRADOURO', e.numero 'N', e.bairro 'BAIRRO', e.cidade 'CIDADE', e.uf 'ESTADO', e.pais 'PAÍS' from CLIENTE c
@@ -85,18 +99,30 @@ inner join PESSOA_FISICA pf on pf.id_cliente = c.id_usuario
 inner join ENDERECO e on e.id_usuario = u.id_usuario
 where u.id_usuario = 8
 
+
+
+
 /*Comando para resetar a tabela*/
 dbcc checkident (usuario, reseed, 0) 
 dbcc checkident (endereco, reseed, 0)
 dbcc checkident (cliente, reseed, 0)
 dbcc checkident (funcionario, reseed, 0)
 
+
+
+
 /*Comando para exibir o id atual da tabela*/
 select IDENT_CURRENT('USUARIO') 
+
+
+
 
 /*MODIFICAÇÕES NAS TABELAS*/
 alter table pessoa_fisica add id_cliente int constraint FK_CLIENTE_PF foreign key(id_cliente) references cliente(id_cliente) on delete cascade;
 alter table CLIENTE add id_usuario int CONSTRAINT FK_ID_USUARIO FOREIGN KEY (id_usuario) REFERENCES USUARIO (id_usuario) on delete cascade;
 alter table endereco add id_usuario int CONSTRAINT FK_ID_USUARIO_ENDERECO FOREIGN KEY (id_usuario) REFERENCES USUARIO (id_usuario) on delete cascade;
+
+
+
 
 delete from USUARIO
